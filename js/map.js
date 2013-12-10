fadb073d386210839f3bee7a5b6aa1f13cb2cdf2
Lindsay Allan <lindsayallan@unknown.home>
2013-12-10 22:14:02 +0000

    adding markers

--- js/map.js
+++ js/map.js
@@ -1,85 +1,45 @@
+
 function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(51.511093, -0.118189),
+      zoom: 11
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  var markers = [];
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
+    
 
-  var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(51.511093, -0.118189),
-      new google.maps.LatLng(51.501093, -0.118189));
-  map.fitBounds(defaultBounds);
+    $.getJSON( "json/bus-stops.json", function( data ) {
+      var items = [];
 
-  // Create the search box and link it to the UI element.
-  var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      $.each(data.markers,function() {
 
-  var searchBox = new google.maps.places.SearchBox(
-    /** @type {HTMLInputElement} */(input));
+        var item = this;
+        latLng = new google.maps.LatLng(this.lat, this.lng); 
 
-  // [START region_getplaces]
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    var places = searchBox.getPlaces();
+         // Creating a marker and putting it on the map
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          title: this.name
+        });
 
+        var infoWindow = new google.maps.InfoWindow({
+          content:'<strong>' + item.id + '</strong>'
+        });
 
-    for (var i = 0, marker; marker = markers[i]; i++) {
-      marker.setMap(null);
-    }
-
-    bounds.extend(place.geometry.location);
-   
-
-     	$.getJSON( "json/bus-stops.json", function( data ) {
-
-     	var items = [];
-		  // For each place, get the icon, place name, and location.
-    markers = [];
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: i.name,
-        position: place.geometry.location
-      });
-
-      markers.push(marker);
-
-      $.each( data, function(  ) {
-    		console.log(markers.id)
- 		 });
       
+        google.maps.event.addListener(marker, "click", function(e) {
+          // infoWindow.setContent(this.name);
+          infoWindow.open(map, marker);
+        });
+
+      });
+    });
+
+  }
 
 
-		 });
-
-   
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 
-    }
-
-    map.fitBounds(bounds);
-  });
-  //[END region_getplaces]
-
-  //Bias the SearchBox results towards places that are within the bounds of the
-  //current map's viewport.
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+ 
